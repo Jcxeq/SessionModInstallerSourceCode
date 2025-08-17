025c54056167885b364c68b18d90ce737e0182bd
jc <58092680+Jcxeq@users.noreply.github.com>
2025-08-16 23:14:11 -0700

    Add files via upload

--- SessionModInstaller.py
+++ SessionModInstaller.py
@@ -39,15 +39,15 @@ sk8 = print("___________________________________________________________________
 print(" ") # empty space 0u0
 sk8 = print("[S] -- Minimal Installation")
 sk8 = print("[K] -- Recommended Installation                                                                                     [P2] -- Page 2")
-sk8 = print("[A] -- [soon]                                                                                                       [?]  - Help")
+sk8 = print("[A] -- Content Creator Installation                                                                                 [?]  - Help")
 sk8 = print("[T] -- [soon]                  ")
 sk8 = print("[E] -- [soon]                  ")
 sk8 = print(" ") # empty space 0u0
 sk8 = print("_______________________________________________________________________________________________________")
 picking = input(Fore.GREEN + "\n>: " + Style.RESET_ALL)
 
-        # directorys   
- 
+        # directorys
+
 UMI = "C:\\SessionModInstaller\\Tools\\Minimal\\UnrealModUnlocker\\Unreal Mod Unlocker Basic.exe" # program location (dont rename/change!)
 SMM = "C:\\SessionModInstaller\\Tools\\Minimal\\Session Mod Manager\\SessionModManager.exe" # program location (dont rename/change!)
 UMIR = "C:\\SessionModInstaller\\Tools\\Recommended\\UnrealModUnlocker\\Unreal Mod Unlocker Basic.exe" # program location (dont rename/change!)
@@ -66,7 +66,7 @@ if picking == "?":
         subprocess.run([RST])
 
 if picking == "s": # minimal tools needed for session mods
-            
+
 
                 os.system("cls") # clearing screen # clearing screen
                 tprint("Session Mod Installer") # makes cool art
@@ -88,7 +88,7 @@ if picking == "s": # minimal tools needed for session mods
                 time.sleep(10)
                 os.system("cls") # clearing screen
                 tprint("Session Mod Installer") # makes cool art
-                print(Fore.GREEN + "\n\nInstrutions:\n\nOpen Steam\nGo to library\nRight click Session Skate Sim, go to manage, and browse local files\nCopy directory into Session mod manager\nAfter, go to third icon, and one by one, drag each zip folder in the Minimal Mods folder, and import\n[ located: C:\\SessionModInstaller\\Tools\\Minimal\\Minimal Mods ]" + Style.RESET_ALL)
+                print(Fore.GREEN + "\n\nInstrutions:\n\nOpen Steam\nGo to library\nRight click Session Skate Sim, go to manage, and browse local files\nCopy directory into Session mod manager\nAfter, go to third icon, and one by one, drag each zip folder in the Minimal Mods folder to the search bar, and click import\n[ located: C:\\SessionModInstaller\\Tools\\Minimal\\Minimal Mods ]" + Style.RESET_ALL)
                 subprocess.run([SMM]) # runs manager
                 os.system("cls") # clearing screen
                 print(Fore.GREEN + "Session Mod Manager is completed!" + Style.RESET_ALL)
@@ -110,7 +110,7 @@ if picking == "s": # minimal tools needed for session mods
 
 
 if picking == "k": # recommended tools needed for session mods
-                
+
 
                 os.system("cls") # clearing screen # clearing screen
                 tprint("Session Mod Installer") # makes cool art
@@ -132,7 +132,49 @@ if picking == "k": # recommended tools needed for session mods
                 time.sleep(10)
                 os.system("cls") # clearing screen
                 tprint("Session Mod Installer") # makes cool art
-                print(Fore.GREEN + "\n\nInstrutions:\n\nOpen Steam\nGo to library\nRight click Session Skate Sim, go to manage, and browse local files\nCopy directory into Session mod manager\nAfter, go to third icon, and one by one, drag each zip folder in the Recommended Mods folder, and import\n[ located: C:\\SessionModInstaller\\Tools\\Recommended\\Recommended Mods ]" + Style.RESET_ALL)
+                print(Fore.GREEN + "\n\nInstrutions:\n\nOpen Steam\nGo to library\nRight click Session Skate Sim, go to manage, and browse local files\nCopy directory into Session mod manager\nAfter, go to third icon, and one by one, drag each zip folder in the Recommended Mods folder to the search bar, and click import, and import\n[ located: C:\\SessionModInstaller\\Tools\\Recommended\\Recommended Mods ]" + Style.RESET_ALL)
+                subprocess.run([SMMR]) # runs manager
+                os.system("cls") # clearing screen
+                tprint("Session Mod Installer") # makes cool art
+                print(Fore.GREEN + "Session Mod Manager is completed!" + Style.RESET_ALL)
+                time.sleep(5)
+
+            # end of manager
+
+                os.system("cls") # clearing screen
+                tprint("Session Mod Installer") # makes cool art
+                print(Fore.GREEN + "\nFinished with recommended installation!" + Style.RESET_ALL)
+                close = input("\nWould like to close the program or continue?: ")
+                print("\nY = Yes -- N = No")
+                if close == "y":
+                        sys.exit(0)
+                elif close == "n":
+                        subprocess.run([RST])
+
+if picking == "a": # content creator tools needed for session mods
+
+
+                os.system("cls") # clearing screen # clearing screen
+                tprint("Session Mod Installer") # makes cool art
+                print(Fore.YELLOW + 'Searching for "Unreal Mod Unlocker"' + Style.RESET_ALL)
+                time.sleep(10)
+                os.system("cls") # clearing screen
+                print("IGNORE LOG BELOW !")
+                subprocess.run([UMIR]) # runs unlocker
+
+            # end of unlocker
+
+                os.system("cls") # clearing screen
+                tprint("Session Mod Installer") # makes cool art
+                print(Fore.GREEN + "Unreal Mod Unlocker is completed!" + Style.RESET_ALL)
+                time.sleep(5)
+                os.system("cls") # clearing screen
+                tprint("Session Mod Installer") # makes cool art
+                print(Fore.YELLOW + 'Searching for "Session Mod Manager"' + Style.RESET_ALL)
+                time.sleep(10)
+                os.system("cls") # clearing screen
+                tprint("Session Mod Installer") # makes cool art
+                print(Fore.GREEN + "\n\nInstrutions:\n\nOpen Steam\nGo to library\nRight click Session Skate Sim, go to manage, and browse local files\nCopy directory into Session mod manager\nAfter, go to third icon, and one by one, drag each zip folder in the Content Creator Mods folder to the search bar, and click import\n[ located: C:\\SessionModInstaller\\Tools\\Content Creator\\Content Creator Mods ]" + Style.RESET_ALL)
                 subprocess.run([SMMR]) # runs manager
                 os.system("cls") # clearing screen
                 tprint("Session Mod Installer") # makes cool art
@@ -152,8 +194,9 @@ if picking == "k": # recommended tools needed for session mods
                         subprocess.run([RST])
 
 
+
 # Page 2
-        
+
 if picking == "p2":
                 os.system("cls") # clearing screen
                 tprint("Session Mod Installer") # makes cool art
@@ -168,7 +211,7 @@ if picking == "p2":
                 sk8 = print("_______________________________________________________________________________________________________")
                 print(" ") # empty space 0u0
                 sk8 = print("[LS] -- Load SMP")
-                sk8 = print("[L?] -- [soon]                                                                                                       [P1] -- Page 1")
+                sk8 = print("[LD] -- Load SessionModInstaller Discord                                                                             [P1] -- Page 1")
                 sk8 = print("[L?] -- [soon]                                                                                                       [?]  - Help")
                 sk8 = print("[L?] -- [soon]                  ")
                 sk8 = print("[L?] -- [soon]                  ")
@@ -196,9 +239,13 @@ if pickingg == "ls":
         print(Fore.YELLOW + "Currently working in progress..." + Style.RESET_ALL)
         time.sleep(2)
         os.system("cls") # clearing screen
-        
 
 
+if pickingg == "ld":
+        SMIdiscord = "https://github.com/Jcxeq/SessionModInstaller/issues"
+        RST = "C:\\SessionModInstaller\\RUN.bat"
+        webbrowser.open(SMIdiscord)
+        subprocess.run([RST])
 
 
 
